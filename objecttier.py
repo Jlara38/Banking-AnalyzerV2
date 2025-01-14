a70e4f37abda3e8860c30651cf700b6f887404c3
Jlara38 <chuplaxlara@gmail.com>
2025-01-13 18:43:53 -0600

    Setting up private class variables and starting functions

--- objecttier.py
+++ objecttier.py
@@ -12,3 +12,39 @@ class Statement:
         self._balance = Bal
         self._check_or_slip = CorS
         
+    @property
+    def Details(self):
+        return self._details
+    
+    @property
+    def Posting_Date(self):
+        return self._posting_date
+    
+    @property
+    def Description(self):
+        return self._description
+    
+    @property
+    def Amount(self):
+        return self._amount
+    
+    @property
+    def Type(self):
+        return self._type
+    
+    @property
+    def Balance(self):
+        return self._balance
+    
+    @property
+    def Check_or_Slip(self):
+        return self._check_or_slip
+    
+    # clear_table_entries
+    #
+    # This command will simply delete the current database in order to create a new one with the 
+    # new csv file data. 
+    #
+    def clear_table_entries(dbConn):
+        dbConn.execute("""DELETE FROM bankinfo""")
+        dbConn.commit()
